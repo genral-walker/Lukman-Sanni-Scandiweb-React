5a820b0cad7ee224e50fb921700470ba9404af86
Lukman <lukmansanni60@gmail.com>
2022-08-23 00:15:36 +0100

    update nav

--- src/components/Navbar/style.ts
+++ src/components/Navbar/style.ts
@@ -2,94 +2,82 @@ import styled from 'styled-components';
 import { desktopFirstResponsiveness } from 'utils/styles';
 
 export const NavbarWrapper = styled.nav`
-  .mobile,
-  .desktop {
-    background: white;
-    display: flex;
-    justify-content: space-between;
-    padding: 20px 67px 8px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100vw;
-    z-index: 1000;
-    ${desktopFirstResponsiveness(730, { padding: '20px 30px 8px' })}
-  }
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 67px 8px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  z-index: 1000;
+  box-shadow: 0 2px 2px -2px rgba(0, 0, 0, 0.2);
+  ${desktopFirstResponsiveness(730, { padding: '20px 30px 8px' })}
 
-  .desktop {
-    z-index: 1100 !important;
-    ${desktopFirstResponsiveness(600, { display: 'none' })}
-    & > div {
-      &:first-of-type {
-        display: flex;
-        & > * {
-          margin-right: 25px;
-          font-weight: 500;
-          cursor: pointer;
-          &:first-of-type {
-            color: #5ece7b;
-            position: relative;
-            &::before {
-              content: '';
-              position: absolute;
-              width: 120%;
-              height: 2px;
-              background-color: currentColor;
-              left: -7px;
-              bottom: -8px;
-              cursor: unset;
-            }
-          }
-        }
-      }
-
-      &:last-of-type {
-        display: flex;
-        align-items: flex-start;
-        select {
-          padding-inline: 7px;
-        }
-
-        div {
+  & > div {
+    &:first-of-type {
+      display: flex;
+      ${desktopFirstResponsiveness(600, { display: 'none' })}
+      & > * {
+        margin-right: 25px;
+        font-weight: 500;
+        cursor: pointer;
+        &:first-of-type {
+          color: #5ece7b;
           position: relative;
-          cursor: pointer;
-          svg {
-            width: 18px;
-            margin-left: 10px;
-            transform: translateY(-2px);
-            z-index: 2;
-          }
-          & > span {
+          &::before {
+            content: '';
             position: absolute;
-            right: -7px;
-            top: -5px;
-            height: 14px;
-            width: 14px;
-            border-radius: 100%;
-            background-color: black;
-            color: white;
-            font-size: 10px;
-            display: grid;
-            place-items: center;
-            padding-bottom: 3px;
+            width: 120%;
+            height: 2px;
+            background-color: currentColor;
+            left: -7px;
+            bottom: -8px;
+            cursor: unset;
           }
         }
       }
     }
 
-    a > svg {
-      align-self: center;
-      margin-left: -65px;
-      ${desktopFirstResponsiveness(1150, { marginLeft: '-70px' })}
-      ${desktopFirstResponsiveness(950, { marginLeft: '-50px' })}
+    &:last-of-type {
+      display: flex;
+      align-items: flex-start;
+      select {
+        padding: 0 2px 0 7px;
+      }
+
+      div {
+        position: relative;
+        cursor: pointer;
+        svg {
+          width: 18px;
+          margin-left: 10px;
+          transform: translateY(-2px);
+          z-index: 2;
+        }
+        & > span {
+          position: absolute;
+          right: -7px;
+          top: -5px;
+          height: 14px;
+          width: 14px;
+          border-radius: 100%;
+          background-color: black;
+          color: white;
+          font-size: 10px;
+          display: grid;
+          place-items: center;
+          padding-bottom: 3px;
+        }
+      }
     }
   }
 
-  .mobile {
-    display: none !important;
+  a > svg {
+    align-self: center;
+    margin: -4px 0 0 -65px;
+    ${desktopFirstResponsiveness(1150, { marginLeft: '-70px' })}
     ${desktopFirstResponsiveness(950, { marginLeft: '-50px' })}
-    & > div {
-      /* background-color: red; */
-    }
+    ${desktopFirstResponsiveness(600, { margin: '-5px 0 0 -5px' })}
   }
 `;
